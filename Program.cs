65cadf8040ea12d48774161fe2c9f8c84a3e6b1e
unknown <=>
2022-04-01 23:27:36 +0400

    end

---
+++ Program.cs
@@ -0,0 +1,283 @@
+﻿using System;
+
+namespace taskall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task1();
+            task2();
+            task3();
+            task4();
+            task5();
+            task6();
+            task7();
+            task8();
+            task9();
+
+
+        }
+        static void task1()
+        {
+            //int a = 14;
+            //int b = 20;
+            //int c = 12;
+            //if (a > b && a > c)
+            //{
+            //    Console.WriteLine($"a boyukdur a-nin deyeri {a}");
+            //}
+            //else if (b > a && b > c)
+            //{
+            //    Console.WriteLine($"b boyukdur b-nin deyeri {b}");
+            //}
+            //else if (c > a && c > b)
+            //{
+            //    Console.WriteLine($"c boyukdur c-nin deyeri {c}");
+            //}
+        }
+        static void task2()
+        {
+            
+                //Console.WriteLine("1 ci ededi daxil edin");
+                //string a = Console.ReadLine();
+                //int sayi1 = int.Parse(a);
+                //Console.WriteLine("2 ci ededi daxil edin");
+                //string b = Console.ReadLine();
+                //int sayi2 = int.Parse(b);
+                //Console.WriteLine("3 ci ededi daxil edin");
+                //string c = Console.ReadLine();
+                //int sayi3 = int.Parse(c);
+                //Console.WriteLine("4 ci ededi daxil edin");
+                //string d = Console.ReadLine();
+                //int sayi4 = int.Parse(d);
+                //Console.WriteLine("5 ci ededi daxil edin");
+                //string e = Console.ReadLine();
+                //int sayi5 = int.Parse(e);
+
+                //double f = (double)(sayi1 + sayi2 + sayi3 + sayi4 + sayi5) / 5;
+                //Console.WriteLine($"ededi ortasi {f}");
+            
+        }
+        static void task3()
+        {
+             //int p = 0;
+            //int s = 0;
+            //Console.WriteLine("eni");
+            //string a = Console.ReadLine();
+            //int sayi1 = int.Parse(a);
+            //Console.WriteLine("uzunluq");
+            //string b = Console.ReadLine();
+            //int sayi2 = int.Parse(b);
+            //p = 2 * (sayi1 + sayi2);
+            //s = (sayi1 * sayi2);
+            //Console.WriteLine($"perimetri {p} sahesi {s}");
+        }
+        static void task4()
+        {
+            //Console.WriteLine("eded daxil edin");
+            //string a = Console.ReadLine();
+            //int sayi1 = int.Parse(a);
+            //int i = 1;
+            //int count = 0;
+            //if (sayi1 > 1)
+            //{
+            //    while (i <= sayi1)
+            //    {
+            //        if (sayi1 % i == 0)
+            //        {
+            //            count++;
+
+            //        }
+            //        i++;
+            //    }
+            //    if (count > 2)
+            //    {
+            //        Console.WriteLine("murekkeb");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("sade");
+            //    }
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("duzgun daxil edin");
+            //}
+        }
+        static void task5()
+        {
+            //Console.WriteLine("ededin mertebesin tapmaq ucun daxil edin");
+            //string a = Console.ReadLine();
+            //int sayi = int.Parse(a);
+            //int count = 0;
+            //while (sayi > 0)
+            //{
+            //    sayi = sayi / 10;
+            //    count++;
+            //}
+            //Console.WriteLine($"mertebesi {count}");
+        }
+        static void task6()
+        {
+            //Console.WriteLine("ededin 3 ve 7  bolunub bolunmemesin bilmek ucun daxil edin");
+            //string a = Console.ReadLine();
+            //int sayi1 = int.Parse(a);
+            //if (sayi1 % 21 == 0)
+            //{
+            //    Console.WriteLine("bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bolunmur");
+            //}
+        }
+        static void task7()
+        {
+            //Console.WriteLine("imtahan qiymetinizi daxil edin");
+            //try
+            //{
+            //    string a = Console.ReadLine();
+            //    int sayi1 = int.Parse(a);
+            //    if (sayi1 >= 0 && sayi1 <= 100)
+            //    {
+            //        if (sayi1 >= 91 && sayi1 <= 100)
+            //        {
+            //            Console.WriteLine($"qiymetiniz  A tebrikler ");
+
+            //        }
+            //        else if (sayi1 >= 81 && sayi1 <= 90)
+            //        {
+            //            Console.WriteLine($"qiymetiniz B bele davam edin ");
+
+            //        }
+            //        else if (sayi1 >= 71 && sayi1 <= 80)
+            //        {
+            //            Console.WriteLine($"qiymetiniz C idare eder ");
+
+            //        }
+            //        else if (sayi1 >= 61 && sayi1 <= 80)
+            //        {
+            //            Console.WriteLine($"qiymetiniz D daha cox calisin ");
+
+            //        }
+            //        else if (sayi1 >= 51 && sayi1 <= 70)
+            //        {
+            //            Console.WriteLine($"qiymetiniz E bele olmaz! ");
+
+            //        }
+            //        else if (sayi1 <= 50)
+            //        {
+            //            Console.WriteLine($"kesildiniz cox tesuf  ");
+
+            //        }
+
+            //    }
+            //    else if (sayi1 < 0)
+            //    {
+            //        Console.WriteLine("edediniz 0dan kicikdir");
+            //    }
+            //    else if (sayi1 > 100)
+            //    {
+            //        Console.WriteLine("ozunden Amerika kesf eleme");
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("ancaq 0 ve 100 arasinda reqem daxil edin");
+                
+            //}
+            
+           
+        }
+        static void task8()
+        {
+            //try
+            //{
+            //    Console.WriteLine("1 ci ededi daxil edin");
+            //    string a = Console.ReadLine();
+            //    int sayi1 = int.Parse(a);
+            //    Console.WriteLine("2 ci ededi daxil edin");
+            //    string b = Console.ReadLine();
+            //    int min = 0;
+            //    int max = 0;
+            //    int result = 0;
+            //    int sayi2 = int.Parse(b);
+            //    if (sayi1 != sayi2)
+            //    {
+            //        if (sayi1 > sayi2)
+            //        {
+            //            min = sayi2;
+            //            max = sayi1;
+            //            while (min <= max)
+            //            {
+            //                result += min;
+            //                min++;
+            //            }
+            //            Console.WriteLine(result);
+            //        }
+            //        else
+            //        {
+            //            min = sayi1;
+            //            max = sayi2;
+            //            while (min <= max)
+            //            {
+            //                result += min;
+            //                min++;
+            //            }
+            //            Console.WriteLine($"cemi {result}");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("beraber ola bilmez");
+            //    }
+            //}
+            //catch (Exception)
+            //{
+            //    Console.WriteLine("ancaq reqem daxil edin!");
+               
+            //}
+            
+        }
+        static void task9()
+        {
+            //try
+            //{
+            //    Console.WriteLine("ededi daxil edin");
+            //    string a = Console.ReadLine();
+            //    int m = int.Parse(a);
+            //    Console.WriteLine("quvveti daxil edin");
+            //    string b = Console.ReadLine();
+            //    int n = int.Parse(b);
+            //    int result = 1;
+            //    int i = 1;
+            //    if (n > 1 && m > 1)
+            //    {
+            //        while (i <= n)
+            //        {
+            //            result = result * m;
+            //            i++;
+
+            //        }
+            //        Console.WriteLine(result);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("duzgun deyer daxil et");
+            //    }
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("reqem daxil edin!!!");
+            //}
+            
+        }
+    }
+}
+    
+
+    
+
